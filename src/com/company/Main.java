7be1b60fab16b89a826025baf87c76d2fd1233f9
Elijah Smith <elijahsmith@Interapts-MacBook-Pro-71.local>
2018-01-30 16:25:23 -0500

    Finished Project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,41 +10,40 @@ public class Main {
         int numberOfGuesses = (0);
 
         final int MAX_GUESS_COUNT = 4;
+        try {
 
 
-        int generatedNumber = (int) Math.ceil(Math.random() * 100);
+            int generatedNumber = (int) Math.ceil(Math.random() * 100);
 
-        do {
+            do {
 
-            System.out.println("Enter your guess:");
-            int userGuess = input.nextInt();
+                System.out.println("Enter your guess:");
+                int userGuess = input.nextInt();
 
-            if (userGuess > generatedNumber) {
-                System.out.println("your number is too high!");
-                numberOfGuesses ++;
-            } else if (userGuess < generatedNumber) {
-                System.out.println("Your number is too low");
-                numberOfGuesses ++;
-            } else if (userGuess == generatedNumber) {
-                System.out.println("Correct!");
-            } else if (numberOfGuesses == MAX_GUESS_COUNT) {
-                System.out.println("You're out of guesses, the correct number was :" + generatedNumber);
-            } else if (userGuess == generatedNumber) {
-                System.out.println("You guessed the correct number!");
-            }
-          } while (numberOfGuesses <= 4);
+                if (userGuess > generatedNumber) {
+                    System.out.println("your number is too high!");
+                    numberOfGuesses++;
+                } else if (userGuess < generatedNumber) {
+                    System.out.println("Your number is too low");
+                    numberOfGuesses++;
+                } else if (userGuess == generatedNumber) {
+                    System.out.println("Correct!");
+                } else if (numberOfGuesses == MAX_GUESS_COUNT) {
+                    System.out.println("You're out of guesses, the correct number was:" + generatedNumber);
+                } else if (userGuess == generatedNumber) {
+                    System.out.println("You guessed the correct number!");
+                }
+            } while (numberOfGuesses <= 4);
+            System.out.println("The corect number was " + generatedNumber);
             System.out.println("Do you want to play again?");
-        String answer = input.nextLine();
-
-        while (answer == "no" && "No"); {
-            System.out.println("Get out of Here!");
-
+        } catch (Exception e) {
+            System.out.println("What kind of answer is that?");
         }
 
-
-        }
 
     }
+}
+
 
 
 
